552e4c70a2e100b2c2526134fde0338f4bd991a0
Rhett Thompson <23347678+kippllo@users.noreply.github.com>
2020-04-03 11:39:56 -0500

    Add files via upload

---
+++ validCharTest.js
@@ -0,0 +1,50 @@
+var btnCheck = document.getElementById('btnCheck');
+var strInput = document.getElementById('inputText');
+var preRules = document.getElementById('rules');
+
+preRules.innerHTML = printValidChars();
+
+btnCheck.addEventListener('click', ()=>{ 
+	var messGood = validCharTest(strInput.value);
+	if(messGood) {
+		alert("Message is good to go!");
+	}
+}, false);
+
+
+function validCharTest(text)
+{
+	var index=0;
+	var returnval=true;
+	var VALIDCHARACTERS=" 1234567890-=qwertyuiop[]\asdfghjkl;''zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?áéíóúüñ¿¡";
+
+	for (index = text.length - 1; index >= 0; --index)
+	{
+		testchar=text.substring(index, index + 1);
+		if(testchar!="\"" && testchar!="\\"  && testchar!="\r\n" && testchar!="\n")
+		{
+			test=VALIDCHARACTERS.indexOf(text.substring(index, index + 1));
+			if (test < 0 && returnval)
+			{
+				alert("Invalid Character found in message body." + `\n\nBad char: ${text.substring(index, index + 1)}\n\nIn part:
+	${text.substring(index-25, index + 25)}`);
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+
+
+function printValidChars() {
+	var VALIDCHARACTERS = " 1234567890-=qwertyuiop[]\asdfghjkl;''zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:ZXCVBNM<>?áéíóúüñ¿¡";
+
+	var str = 'The only valid chars are: ';
+	for(var i=0; i<VALIDCHARACTERS.length; i++) {
+		str += '\n" ' + VALIDCHARACTERS[i] + ' "';
+	}
+
+	return str;
+}
